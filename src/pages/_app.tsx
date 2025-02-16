a37fae8c91e3a5c3e99d42af0861dbdc50870fbf
SHJ <hads2456@naver.com>
2025-02-16 23:05:24 +0900

    네비게이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const rotuer = useRouter();
+
+  const onClickButton = () => {
+    rotuer.push("/test");
+  };
+
   return (
     <div>
-      <header>글로벌 헤더</header>
-    <Component {...pageProps} />
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button type="button" onClick={onClickButton}>
+            /test 페이지로 이동
+          </button>
+        </div>
+      </header>
+      <Component {...pageProps} />
     </div>
-  )
+  );
 }
