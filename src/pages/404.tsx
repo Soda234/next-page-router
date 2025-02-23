42107bd49ab1d941606512b4fb0ccfc2e39c7c60
SHJ <hads2456@naver.com>
2025-02-23 18:58:02 +0900

    UI 구현현

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>존재 하지 않는 페이지</div>;
+  return <div>존재하지 않는 페이지입니다</div>;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import GlobalLayout from "@/components/GlobalLayout";
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
@@ -14,7 +14,12 @@ export default function App({
 }: AppProps & {
   Component: NextPageWithLayout;
 }) {
-  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+  const getLayout =
+    Component.getLayout ?? ((page: ReactNode) => page);
 
-  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
+  return (
+    <GlobalLayout>
+      {getLayout(<Component {...pageProps} />)}
+    </GlobalLayout>
+  );
 }
